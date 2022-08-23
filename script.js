7ddfeb31a0ebe3413e62f3ab39b013f1c80183e1
suromonika <suro.monika@gmail.com>
2022-08-23 16:59:57 +0200

    Added style and added button functionality

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const input = document.querySelector('#guest-name');
 const guestList = document.querySelector('#guest-list');
-
 let guestListArray = [];
 
-function createElement() {
+function createList() {
   for (let i = 0; guestListArray.length > i; i++) {
     const newItem = document.createElement('div');
-
+    newItem.classList.add('new-guest');
     newItem.appendChild(document.createTextNode(guestListArray[i]));
     guestList.appendChild(newItem);
   }
@@ -14,6 +13,36 @@ function createElement() {
 
 document.querySelector('#add-top').addEventListener('click', (e) => {
   guestListArray.unshift(input.value);
+  guestList.replaceChildren();
+  createList();
 
-  createElement();
+  input.value = '';
+});
+
+document.querySelector('#add-bottom').addEventListener('click', (e) => {
+  guestListArray.push(input.value);
+  guestList.replaceChildren();
+  createList();
+  input.value = '';
+});
+
+document.querySelector('#remove-last').addEventListener('click', (e) => {
+  guestListArray.pop(input.value);
+  guestList.replaceChildren();
+  createList();
+  input.value = '';
+});
+
+document.querySelector('#remove-first').addEventListener('click', (e) => {
+  guestListArray.shift(input.value);
+  guestList.replaceChildren();
+  createList();
+  input.value = '';
+});
+
+document.querySelector('#reverse').addEventListener('click', (e) => {
+  guestListArray.reverse(input.value);
+  guestList.replaceChildren();
+  createList();
+  input.value = '';
 });
